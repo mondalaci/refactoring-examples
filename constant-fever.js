cb13e6b5ec58fc8c74cd258c9f56527716552acf
László Monda <laci@monda.hu>
2015-04-14 23:30:18 +0200

    Create constant-fever.js

---
+++ constant-fever.js
@@ -0,0 +1,22 @@
+// LOUSY CODE:
+
+due += 8 * 24 * 3600e3; // eight days
+
+// SUPREME CODE:
+
+// duration.js:
+
+var DURATION = function() {
+    self = {};
+    self.MILLISECONDS_IN_SECOND = 1000;
+    self.MILLISECONDS_IN_MINUTE = 60 * self.MILLISECONDS_IN_SECOND;
+    self.MILLISECONDS_IN_HOUR   = 60 * self.MILLISECONDS_IN_MINUTE;
+    self.MILLISECONDS_IN_DAY    = 24 * self.MILLISECONDS_IN_HOUR;
+    self.MILLISECONDS_IN_MONTH  = 30 * self.MILLISECONDS_IN_DAY;
+    self.MILLISECONDS_IN_YEAR   = 365 * self.MILLISECONDS_IN_MONTH;
+    return self;
+};
+
+// myCode.js:
+
+due += 8 * DURATION.MILLISECONDS_IN_DAY;
